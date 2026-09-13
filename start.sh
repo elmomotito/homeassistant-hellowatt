5673f00850dd6a88712faa62563a76e3ccbca89d
elmomotito <145811435+elmomotito@users.noreply.github.com>
2026-09-13 14:49:04 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-printf '0 8 * * * /usr/local/bin/python /app/run.py >> /proc/1/fd/1 2>> /proc/1/fd/2\n' > /etc/crontabs/root
-if ! python /app/run.py --startup; then
-    echo 'Essai au démarrage en échec ; le passage quotidien reste programmé.'
-fi
-exec crond -f -l 8
